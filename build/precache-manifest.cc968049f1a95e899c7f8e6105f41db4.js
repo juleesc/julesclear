0e7bf06137b9c389b4a4f953048e1df0e520f141
Jules CLEAR <jules.clear@yahoo.fr>
2020-11-04 20:35:04 +0100

    manifest jso,

---
+++ build/precache-manifest.cc968049f1a95e899c7f8e6105f41db4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d21d59bb6a78c3dbc246c11391ed6b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6ec406881680d5e15ac",
+    "url": "/static/css/2.b6e3a137.chunk.css"
+  },
+  {
+    "revision": "16d6e414898bb7d6d866",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "b6ec406881680d5e15ac",
+    "url": "/static/js/2.167ae18f.chunk.js"
+  },
+  {
+    "revision": "16d6e414898bb7d6d866",
+    "url": "/static/js/main.387eaefe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/julesclear/precache-manifest.c4a9e58cf153794865b47a8670598f1f.js"
+  "/precache-manifest.cc968049f1a95e899c7f8e6105f41db4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/julesclear/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
