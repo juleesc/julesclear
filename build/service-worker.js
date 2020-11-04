9f7fe7ba46319b0ce4a16b4a504b99c5413ad9f5
Jules CLEAR <jules.clear@yahoo.fr>
2020-11-04 20:04:01 +0100

    testd

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "julesclear.fr/precache-manifest.9357079ce77d5d325a5088d5ea73e2cd.js"
+  "/julesclear/precache-manifest.c4a9e58cf153794865b47a8670598f1f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("julesclear.fr/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/julesclear/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
