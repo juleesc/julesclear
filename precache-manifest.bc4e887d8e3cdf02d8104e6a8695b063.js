0dff958b63fb07db233edbf54b5cdac0885597bd
Jules CLEAR <jules.clear@yahoo.fr>
2020-11-04 17:58:51 +0100

    assets

---
+++ precache-manifest.bc4e887d8e3cdf02d8104e6a8695b063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2aa44266e2e1d741b1e6f23f28f76a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a8526fef237e2e03f1d",
+    "url": "/static/css/2.b6e3a137.chunk.css"
+  },
+  {
+    "revision": "aee098ee4f5d592a5d57",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "9a8526fef237e2e03f1d",
+    "url": "/static/js/2.7de5163c.chunk.js"
+  },
+  {
+    "revision": "aee098ee4f5d592a5d57",
+    "url": "/static/js/main.b488822a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
